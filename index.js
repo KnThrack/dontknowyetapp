92872ff5f6eeab7ea28ab10723cef1073c94bbab
Kay Degro <kay.degro@ecenta.com>
2019-07-04 16:37:25 +0200

    0Auth

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Import express
 let express = require('express');
+let jwt = require("express-jwt");
+let jwksRsa = require("jwks-rsa");
 // Import Body parser
 let bodyParser = require('body-parser');
 // Import Mongoose
@@ -9,6 +11,34 @@ let cors = require("cors");
 
 // Initialize the app
 let app = express();
+
+// Set up Auth0 configuration
+const authConfig = {
+    domain: "shrill-brook-0201.eu.auth0.com",
+    audience: "https://notsureyetapp.herokuapp.com/api"
+};
+
+// Define middleware that validates incoming bearer tokens
+// using JWKS from dev-dontknowyet.eu.auth0.com
+const checkJwt = jwt({
+    secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+    }),
+
+    audience: authConfig.audience,
+    issuer: `https://${authConfig.domain}/`,
+    algorithm: ["RS256"]
+});
+
+// Define an endpoint that must be called with an access token
+app.get("/api/external", checkJwt, (req, res) => {
+    res.send({
+        msg: "Your Access Token was successfully validated!"
+    });
+});
 
 // Import routes
 let apiRoutes = require("./api-routes")
@@ -20,13 +50,14 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Connect to Mongoose and set connection variable
-mongoose.connect(process.env.MONGO, {useNewUrlParser: true});
+mongoose.connect(process.env.MONGO, { useNewUrlParser: true });
 var db = mongoose.connection;
 
 // Setup server port
 var port = process.env.PORT || 8080;
 // Send message for default URL
 app.get('/', (req, res) => res.send('Hello World with Express'));
+
 // Use Api routes in the App
 app.use('/api', apiRoutes)
 // Launch app to listen to specified port
