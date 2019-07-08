be6bcc144803d237b39f5c436cd4599eb54a8e63
Kay Degro <kay.degro@ecenta.com>
2019-07-08 15:40:27 +0200

    kk

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ var port = process.env.PORT || 8080;
 app.get('/', checkJwt, (req, res) => res.send('Hello World with Express'));
 
 // test sth 
-app.use(function (req, res, next) {
+app.use(checkJwt, (req, res, next) => {
     console.log('Hello Time:', Date.now());
 
     if (req.headers.authorization) {
